f0284172cc99f26f16cbf5cc4c4798bedde98405
Erick Aiken <44817305+ErickAiken@users.noreply.github.com>
2023-06-01 21:13:01 -0700

    Added basic camera script
    can now control camera with mouse, still need to add movement control from clicks and camera direction

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("isRunningForward", false);
+        animator.SetBool("isRunningBackward", false);
+    
+        if(Input.GetKey("a") || Input.GetKey("w") || Input.GetKey("d"))
+        {
+            animator.SetBool("isRunningForward", true);
+            animator.SetBool("isRunningBackward", false);
+        }
+        if(Input.GetKey("s"))
+        {
+            animator.SetBool("isRunningForward", false);
+            animator.SetBool("isRunningBackward", true);
+        }
+    }
+}
