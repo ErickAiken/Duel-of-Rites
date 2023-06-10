255aa907c065e2e4fa06590a75cfdc910879d5bd
Erick Aiken <44817305+ErickAiken@users.noreply.github.com>
2023-06-10 13:56:19 -0700

    assets

--- Assets/Scripts/WindmillSpin.cs
+++ Assets/Scripts/WindmillSpin.cs
@@ -7,17 +7,13 @@ public class WindmillSpin : MonoBehaviour
 
     public float spinSpeed = 1.0f;
     public Transform bladeObject;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 rotationDirection = new Vector3(0.0f, 0.0f, 0.0f);
+    public float rot = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        rot += spinSpeed * Time.deltaTime;
+        transform.localRotation = Quaternion.Euler(rot,0f,0f);
     }
 }
