ebefd8156480d587e6d08b78f8276d18c6483482
Erick Aiken <44817305+ErickAiken@users.noreply.github.com>
2023-06-11 12:24:57 -0700

    major UI updates

---
+++ Assets/Scripts/InterfaceSettings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InterfaceSettings_ 
+{
+    // Set of defaults 
+    public static float hotkeyTextPosX = -30.0f;
+    public static float hotkeyTextPosY = 0.0f;
+    public static float hotkeyTextWidth = 25.0f;
+    public static float hotkeyTextHeight = 40.0f;
+
+}
