733acfcf2924621999e77702156db216d5177ad1
Erick Aiken <44817305+ErickAiken@users.noreply.github.com>
2023-06-15 22:46:30 -0700

    began work on action bar transitions

--- Assets/Scripts/AbilityBarTransitions.cs
+++ Assets/Scripts/AbilityBarTransitions.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CastAbility : MonoBehaviour
+public class AbilityBarTransitions : MonoBehaviour
 {
     private GameObject spellSlot1;
 
@@ -17,6 +17,11 @@ public class CastAbility : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Make the transition happen when 1 is pressed
+        spellSlot1.GetComponent<DuloGames.UI.UIHighlightTransition>().enabled = true;
+        if(Input.GetKey(KeyCode.Alpha1)){
+            spellSlot1.GetComponent<DuloGames.UI.UIHighlightTransition>().enabled = false;
+        }
         
     }
 }
