ea1179bb9332a5c1324c7fe46aac51973b5c2f37
Erick Aiken <44817305+ErickAiken@users.noreply.github.com>
2023-06-15 21:23:34 -0700

    gitignore

---
+++ Assets/Scripts/CastAbility.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastAbility : MonoBehaviour
+{
+    private GameObject spellSlot1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spellSlot1 = GameObject.Find("Spell Slot 1");
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
